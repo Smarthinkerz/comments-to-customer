47114da4c70d9fa26a42c8671a0ed8ba29c3a1e0
CommentCustomer Deploy <deploy@commentcustomer.ai>
2026-05-14 11:50:22 +0400

    deploy: client/src/App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,36 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import { useEffect } from "react";
+
+function OrbitalRedirect() {
+  useEffect(() => {
+    window.location.href = "/orbital.html";
+  }, []);
+  return null;
+}
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/orbital.html" component={OrbitalRedirect} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
