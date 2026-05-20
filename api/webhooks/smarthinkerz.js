9ba128d1b14237936a2a023c906a1f96420b831d
Smarthinkerz <fathi.alriyami@smarthinkerz.com>
2026-05-20 09:09:46 +0400

    Self-contained webhook handler (require server.cjs directly)

--- api/webhooks/smarthinkerz.js
+++ api/webhooks/smarthinkerz.js
@@ -1,1 +1,13 @@
-module.exports = require('../all.js');
+'use strict';
+const { requestHandler, ensureInit } = require('../../server.cjs');
+
+module.exports = async (req, res) => {
+    try {
+        await ensureInit();
+    } catch (err) {
+        res.statusCode = 503;
+        res.setHeader('Content-Type', 'application/json');
+        return res.end(JSON.stringify({ ok: false, error: 'init_failed' }));
+    }
+    return requestHandler(req, res);
+};
